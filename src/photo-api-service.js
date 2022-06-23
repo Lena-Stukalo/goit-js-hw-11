4cc15bf160a7564c595fb231b5d32189f23e3158
Lena-Stukalo <68201887+Lena-Stukalo@users.noreply.github.com>
2022-06-24 00:42:32 +0300

    update

--- src/photo-api-service.js
+++ src/photo-api-service.js
@@ -1,4 +1,5 @@
 const axios = require('axios').default;
+import Notiflix from 'notiflix';
 
 export default class Photo {
   constructor() {
@@ -6,14 +7,16 @@ export default class Photo {
     this.page = 1;
   }
   async fetchPhotos() {
-    return await axios
-      .get(
+    try {
+      const response = await axios.get(
         `https://pixabay.com/api/?key=28164685-e508b46b7d4362311384dafbb&q=${this.searchQuery}&per_page=40&page=${this.page}&image_type=photo&orientation=horizontal&safesearch=true`
-      )
-      .then(response => {
-        this.page += 1;
-        return response.data.hits;
-      });
+      );
+      const hits = await response.data.hits;
+      this.page += 1;
+      return hits;
+    } catch {
+      Notiflix.Notify.failure('Sorry, we have API error. Please try again.');
+    }
   }
   get query() {
     return this.searchQuery;
